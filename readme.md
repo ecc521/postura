927ef66f487ced4b86ad4d70612afea763b61541
ecc521 <42424091+ecc521@users.noreply.github.com>
2022-07-01 14:27:11 -0400

    Very initial design

---
+++ readme.md
@@ -0,0 +1,13 @@
+### Installation
+
+Make sure to set flags as required by https://github.com/capacitor-community/bluetooth-le
+- IOS requires a message explaining what bluetooth is used for. (We use "Bluetooth is used to connect and sync with your wearable device. ")
+- Android requires a declaration that scanning never used for location, or location permissions to be requested. 
+
+
+# Build Assets
+Run 
+```
+cordova-res ios --skip-config --copy
+cordova-res android --skip-config --copy
+```
---
+++ src/pages/Details.tsx
@@ -0,0 +1,25 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Details.css';
+
+const Tab2: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Details</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Details</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Tab 2 page" />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
